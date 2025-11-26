d51cedeaeb2cf5d57adbccba952e83316f0ae95c
DadMoose MacBook 15 <dadmoose@ya.ru>
2025-11-26 11:13:10 +0300

    test: добавлены переменные alias, команды cypress для теста конструктора

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,3 +1,16 @@
+const selectors = {
+  modalTitle: 'Детали ингредиента',
+  modalHeader: 'h3',
+  modalRoot: '#modals',
+  constructorItem: '.constructor-element'
+};
+
+const ingredientNames = {
+  bun: 'Булка тестовая',
+  main: 'Котлета тестовая',
+  sauce: 'Соус тестовый'
+};
+
 describe('Конструктор', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/ingredients', {
@@ -13,28 +26,37 @@ describe('Конструктор', () => {
   });
 
   it('добавляет булку и начинку в конструктор', () => {
-    cy.contains('li', 'Булка тестовая').contains('Добавить').click();
-    cy.contains('li', 'Котлета тестовая').contains('Добавить').click();
-    cy.contains('li', 'Соус тестовый').contains('Добавить').click();
+    cy.addIngredient(ingredientNames.bun);
+    cy.addIngredient(ingredientNames.main);
+    cy.addIngredient(ingredientNames.sauce);
 
-    cy.contains('Булка тестовая (верх)').should('exist');
-    cy.contains('Булка тестовая (низ)').should('exist');
-    cy.contains('.constructor-element', 'Котлета тестовая').should('exist');
-    cy.contains('.constructor-element', 'Соус тестовый').should('exist');
+    cy.contains(`${ingredientNames.bun} (верх)`).should('exist');
+    cy.contains(`${ingredientNames.bun} (низ)`).should('exist');
+    cy.contains(selectors.constructorItem, ingredientNames.main).should(
+      'exist'
+    );
+    cy.contains(selectors.constructorItem, ingredientNames.sauce).should(
+      'exist'
+    );
   });
 
   it('открывает и закрывает модальное окно ингредиента', () => {
-    cy.contains('li', 'Соус тестовый').click();
-    cy.contains('Детали ингредиента').should('exist');
-    cy.contains('.text_type_main-medium', 'Соус тестовый').should('exist');
+    cy.openIngredientModal(ingredientNames.sauce);
+    cy.contains(selectors.modalTitle).should('exist');
+    cy.contains('.text_type_main-medium', ingredientNames.sauce).should(
+      'exist'
+    );
 
-    cy.contains('h3', 'Детали ингредиента').parent().find('button').click();
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains(selectors.modalHeader, selectors.modalTitle)
+      .parent()
+      .find('button')
+      .click();
+    cy.contains(selectors.modalTitle).should('not.exist');
 
-    cy.contains('li', 'Булка тестовая').click();
-    cy.contains('Детали ингредиента').should('exist');
-    cy.get('#modals').children().last().click({ force: true });
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains('li', ingredientNames.bun).click();
+    cy.contains(selectors.modalTitle).should('exist');
+    cy.get(selectors.modalRoot).children().last().click({ force: true });
+    cy.contains(selectors.modalTitle).should('not.exist');
   });
 
   it('оформляет заказ и очищает конструктор', () => {
@@ -42,15 +64,15 @@ describe('Конструктор', () => {
       'createOrder'
     );
 
-    cy.contains('li', 'Булка тестовая').contains('Добавить').click();
-    cy.contains('li', 'Котлета тестовая').contains('Добавить').click();
-    cy.contains('li', 'Соус тестовый').contains('Добавить').click();
+    cy.addIngredient(ingredientNames.bun);
+    cy.addIngredient(ingredientNames.main);
+    cy.addIngredient(ingredientNames.sauce);
     cy.contains('Оформить заказ').click();
 
     cy.wait('@createOrder');
     cy.contains('123456').should('exist');
 
-    cy.get('#modals').children().last().click({ force: true });
+    cy.get(selectors.modalRoot).children().last().click({ force: true });
     cy.contains('Выберите булки').should('exist');
     cy.contains('Выберите начинку').should('exist');
   });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,22 @@
 //     }
 //   }
 // }
+
+Cypress.Commands.add('addIngredient', (name: string) => {
+  cy.contains('li', name).contains('Добавить').click();
+});
+
+Cypress.Commands.add('openIngredientModal', (name: string) => {
+  cy.contains('li', name).click();
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      addIngredient(name: string): Chainable<HTMLElement>;
+      openIngredientModal(name: string): Chainable<HTMLElement>;
+    }
+  }
+}
+
+export {};
